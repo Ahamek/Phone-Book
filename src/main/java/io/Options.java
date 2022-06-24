bd9b9223218919a1854aa39e69d7a2a20edd9f29
Michal Malinowski <malinowski.michalpl@gmail.com>
2022-06-24 16:50:19 +0200

    Add io options for program

---
+++ src/main/java/io/Options.java
@@ -0,0 +1,39 @@
+package io;
+
+import java.util.NoSuchElementException;
+
+public enum Options {
+    ADD_CONTACT(0, "Dodaj kontakt"),
+    SEARCH_BY_NAME(1, "Szukaj po nazwie"),
+    SEARCH_BY_TEL(2, "Szukaj po telefonie"),
+    DELETE(3, "Usuñ"),
+    CLOSE(4, "Koniec");
+
+    private final int shortcut;
+    private final String description;
+
+    Options(int shortcut, String description) {
+        this.shortcut = shortcut;
+        this.description = description;
+    }
+
+    public static Options convertToOption(int option){
+        if (option >= values().length || option < 0){
+            throw new NoSuchElementException("There is no such option ");
+        }
+        return values()[option];
+    }
+
+    public int getShortcut() {
+        return shortcut;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return shortcut + " - " + description;
+    }
+}
